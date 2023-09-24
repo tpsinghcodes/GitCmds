62a05ef4ba4a84c17fec971974dab605c95ad1b5
tpsinghcodes <137421551+tpsinghcodes@users.noreply.github.com>
2023-09-24 10:56:34 +0530

    Create vscode.userstting.js

---
+++ vscode.userstting.js
@@ -0,0 +1,35 @@
+{
+    "workbench.iconTheme": "material-icon-theme",
+    "editor.minimap.renderCharacters": false,
+    "editor.minimap.autohide": true,
+    "editor.tabSize": 2,
+    "editor.fontSize": 20,
+    "editor.fontFamily": "MonoLisa",
+    //"editor.fontFamily": "Comic Code",
+    "editor.wordWrap": "on",
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
+    "editor.formatOnSave": true,
+    "editor.codeActionsOnSave": {
+      "source.fixAll.eslint": true,
+      "source.addMissingImports": true
+    },
+    "[typescriptreact]": {
+      "editor.defaultFormatter": "esbenp.prettier-vscode"
+    },
+    "terminal.integrated.fontSize": 16,
+    "workbench.colorTheme": "Dracula",
+    "terminal.integrated.fontFamily": "monospace",
+    "todohighlight.isEnable": true,
+    "window.zoomLevel": 0,
+    "[Log]": {
+      "editor.fontSize": 22
+    },
+    "files.exclude": {
+      "**/.git": false
+    },
+    "emmet.includeLanguages": {
+      "javascript": "javascriptreact",
+      "typescript": "typescriptreact"
+    },
+    "git.autofetch": true
+}
