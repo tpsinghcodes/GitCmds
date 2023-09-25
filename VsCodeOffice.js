b99b7368d85b376aed57f13d9aab81f10ae74ebd
tpsinghcodes <137421551+tpsinghcodes@users.noreply.github.com>
2023-09-25 01:31:57 -0400

    Create VsCodeOffice.js

---
+++ VsCodeOffice.js
@@ -0,0 +1,31 @@
+{
+  "workbench.colorTheme": "Dracula",
+  "workbench.iconTheme": "material-icon-theme",
+  "editor.minimap.renderCharacters": false,
+  "editor.minimap.autohide": true,
+  "editor.fontSize": 23,
+  "editor.tabSize": 2,
+  "editor.fontFamily": "MonoLisa-Regular",
+  "editor.wordWrap": "on",
+  "editor.defaultFormatter": "esbenp.prettier-vscode",
+  "editor.formatOnSave": true,
+  "editor.codeActionsOnSave": {
+    "source.fixAll.eslint": true,
+    "source.addMissingImports": true
+  },
+  "[typescriptreact]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "terminal.integrated.fontSize": 16,
+  "terminal.integrated.fontFamily": "monospace",
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact",
+    "typescript": "typescriptreact"
+  },
+  "files.exclude": {
+    "**/.git": false
+  },
+  "editor.quickSuggestions": {
+    "strings": true
+  }
+}
